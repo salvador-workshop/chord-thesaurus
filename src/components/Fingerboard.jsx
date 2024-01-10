c5246084a270c2921034bb529213a287e6eb0b4b
RJ Salvador <rjsalvadorr@gmail.com>
2024-01-09 19:19:12 -0500

    cleanup tings

--- src/components/Fingerboard.jsx
+++ src/components/Fingerboard.jsx
@@ -3,32 +3,30 @@ import { getFbSectionPercentages, isOdd} from '../fingerboard-utils';
 import PitchMarker from './PitchMarker';
 
 const buildSvg = (maxPositions) => {
-    // const lines = [100, 200, 300];
-    const fbLength = 2400;
-    const fbWidth = 300;
-    const numSections = maxPositions * 2;
-    const fbSections = getFbSectionPercentages(numSections);
+    const fbLength = 1600;
+    const fbWidth = 200;
 
+    const numSections = maxPositions * 2;
+    const fbSectionLengths = getFbSectionPercentages(numSections);
     const addedFbSections = [];
     let total = 0;
-    for (let i = 0; i < fbSections.length; i++) {
-        addedFbSections.push(total + fbSections[i]);
-        total += fbSections[i];
+    for (let i = 0; i < fbSectionLengths.length; i++) {
+        addedFbSections.push(total + fbSectionLengths[i]);
+        total += fbSectionLengths[i];
     }
-
-    const fbSectionObjects = addedFbSections.map((fbDistance, idx) => {
+    const fbSections = addedFbSections.map((distanceFromNut, idx) => {
         return {
             isVisible: isOdd(idx),
-            fbDistance: fbDistance,
+            distFromNut: distanceFromNut,
         }
     })
 
     return (
-        <svg width={fbLength} height={fbWidth} viewBox="0 0 2400 300" stroke="#aa0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg width={fbLength} height={fbWidth} viewBox={`0 0 ${fbLength} ${fbWidth}}`} stroke="#aa0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <rect x="0" y="0" width={fbLength} height={fbWidth} fill="none" stroke-width="8.33"></rect>
-            {fbSectionObjects.map(sect => {
-                const calcPos = sect.fbDistance / 100 * fbLength;
-                console.log(sect.fbDistance, calcPos);
+            {fbSections.map(sect => {
+                const calcPos = sect.distFromNut / 100 * fbLength;
+                console.log(sect.distFromNut, calcPos);
                 if (sect.isVisible) {
                     return <line key={calcPos} x1={`${calcPos}`} y1="0" x2={`${calcPos}`} y2={fbWidth} stroke-width="4.16"></line>
                 } else {
--- src/stories/Fingerboard.stories.js
+++ src/stories/Fingerboard.stories.js
@@ -16,6 +16,6 @@ export const Default = {
     isFretted: false,
     isReversed: false,
     stringPitches: [50, 40, 30, 20],
-    maxPositions: 24,
+    maxPositions: 18,
   },
 };
