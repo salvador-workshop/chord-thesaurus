abcd59ebae9c8ba8000e04f0cad509465ba5eb32
RJ Salvador <rjsalvadorr@gmail.com>
2024-01-30 00:40:54 -0500

    post-mirror cleanup

--- src/components/FingerboardGuide.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function FingerboardGuide(guideMode) {
-
-    return (
-        <div className={`fingerboard-guide ${guideMode}`}>FingerboardGuide</div>
-    )
-}
