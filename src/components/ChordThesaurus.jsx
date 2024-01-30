2dde39869da0ef43d11309529794421f5d6cee69
RJ Salvador <rjsalvadorr@gmail.com>
2024-01-30 15:04:08 -0500

    refined json data structure, more cleanup

--- src/components/ChordThesaurus.jsx
+++ src/components/ChordThesaurus.jsx
@@ -1,27 +1,34 @@
 import { Fragment } from 'react';
-import ChordData from '../data/chords.json';
 import {
     initialSettings,
 } from '../style-utils';
 import FbDiagramGroup from './FbDiagramGroup';
 
-export default function ChordThesaurus() {
+import majChords from '../data/maj-chords.json';
+import minChords from '../data/min-chords.json';
+import dimChords from '../data/dim-chords.json';
 
-    const chordSections = ChordData.sections.map(section => {
-        return (
-            <Fragment key={section.id}>
-                <hgroup>
-                    {section.title && <h2>{section.title}</h2>}
-                    {section.subtitle && <h3>{section.subtitle}</h3>}
-                    {section.description && <p>{section.description}</p>}
-                </hgroup>
-                <FbDiagramGroup sectionId={section.id} settings={initialSettings} chordSection={section}/>
-            </Fragment>
-        )
-    });
+export default function ChordThesaurus() {
+    const renderChordChapter = (chordChapter) => {
+        return chordChapter.sections.map(section => {
+            return (
+                <Fragment key={section.id}>
+                    <hgroup>
+                        {section.title && <h2>{section.title}</h2>}
+                        {section.subtitle && <h3>{section.subtitle}</h3>}
+                        {section.description && <p>{section.description}</p>}
+                    </hgroup>
+                    <FbDiagramGroup sectionId={section.id} settings={initialSettings} chordSection={section}/>
+                </Fragment>
+            )
+        });
+    }
 
     return <>
         <h1>Guitar Chord Thesaurus</h1>
-        {chordSections}
+
+        {renderChordChapter(majChords)}
+        {renderChordChapter(minChords)}
+        {renderChordChapter(dimChords)}
     </>;
 }
