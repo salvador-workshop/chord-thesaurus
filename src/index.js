4eea17c5945c3b61707f626d61a68ecd6b1407e0
RJ Salvador <salvador.workshop@gmail.com>
2024-06-30 13:44:52 -0400

    Big cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
-import ChordThesaurus from "./components/ChordThesaurus";
 import Guitarragrams from "./components/Guitarragrams";
 
 const root = createRoot(document.getElementById("app"));
-// root.render(<ChordThesaurus />);
 root.render(<Guitarragrams />);
